733871513392e80808ae3b7ef3a9d6ed4432dda3
Siddharth Subramanian <sidsbrmnn@gmail.com>
2019-11-10 15:26:51 +0530

    routes: error flashing included

--- routes/index.js
+++ routes/index.js
@@ -7,52 +7,51 @@ const User = require('../models/user');
 const router = express.Router();
 
 router.get('/', ensureLoggedOut('/dashboard'), (req, res) => {
-  res.render('index', { title: 'Home' });
+	res.render('index', { title: 'Home' });
 });
 
 router.get('/dashboard', ensureLoggedIn('/login'), (req, res) => {
-  res.render('dashboard', { title: 'Dashboard', user: req.user });
+	res.render('dashboard', { title: 'Dashboard', user: req.user });
 });
 
 router.get('/login', ensureLoggedOut('/dashboard'), (req, res) => {
-  res.render('login', { title: 'Login' });
+	res.render('login', { title: 'Login', message: req.flash('error') });
 });
 
 router.get('/profile', ensureLoggedIn('/login'), (req, res) => {
-  res.render('profile', { user: req.user });
+	res.render('profile', { user: req.user });
 });
 
 router.post('/profile', ensureLoggedIn('/login'), async (req, res) => {
-  const user = await User.findOneAndUpdate(
-    { email: req.body.email },
-    { name: req.body.name },
-    { new: true }
-  );
+	const user = await User.findOneAndUpdate(
+		{ email: req.body.email },
+		{ name: req.body.name },
+		{ new: true }
+	);
 
-  req.login(user, err => {
-    if (err)
-      return res.render('profile', {
-        error: 'Something went wrong',
-        user: req.user
-      });
-  });
+	req.login(user, err => {
+		if (err)
+			return res.render('profile', {
+				error: 'Something went wrong',
+				user: req.user
+			});
+	});
 
-  res.render('profile', { success: 'Profile saved', user: req.user });
+	res.render('profile', { success: 'Profile saved', user: req.user });
 });
 
 router.post(
-  '/login',
-  passport.authenticate('local', {
-    failureRedirect: '/login'
-  }),
-  (req, res) => {
-    res.redirect('/dashboard');
-  }
+	'/login',
+	passport.authenticate('local', {
+		successReturnToOrRedirect: '/',
+		failureRedirect: '/login',
+		failureFlash: true
+	})
 );
 
 router.get('/logout', (req, res) => {
-  req.logout();
-  res.redirect('/login');
+	req.logout();
+	res.redirect('/login');
 });
 
 module.exports = router;
